9b50ef7e465907a1c681b8326915eba75c049900
Murat Efe Guney <murat.e.guney@intel.com>
2015-10-31 09:12:59 -0700

    Corrections to the unpack SSE

--- internal/unpack.h
+++ internal/unpack.h
@@ -159,6 +159,8 @@ void UnpackResult(ResultBlockType* dst, const PackedResultType& src, int depth,
 
 #ifdef GEMMLOWP_NEON
 #include "unpack_neon.h"
+#elif defined (GEMMLOWP_SSE4)
+#include "unpack_SSE.h"
 #endif
 
 #endif  // GEMMLOWP_INTERNAL_UNPACK_H_
